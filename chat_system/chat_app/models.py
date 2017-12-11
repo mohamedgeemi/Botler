feaa96901741e77641d4d29d9aa88479549aef97
Mohamed Gamal <mohamed.gamal.soliman@gmail.com>
2017-12-11 23:02:27 +0200

    Enhance message model
    Saving the message_html instead of keep rendering it

--- chat_system/chat_app/models.py
+++ chat_system/chat_app/models.py
@@ -6,6 +6,7 @@ class Message(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField(max_length=3000)
+    message_html = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
